e2794d2cfec5bc4f0c90509a4e1d1ca443983ba5
Angeladaddy <sjtusunchenge@gmail.com>
2026-02-05 22:46:14 +0000

    merge two npm installs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        include: ['test/**/*.{test,spec}.ts', 'test/**/*test_*.ts'],
+        include: ['backend/test/**/*.{test,spec}.ts', 'backend/test/**/*test_*.ts'],
         globals: true,
         environment: 'node',
         testTimeout: 30000,
-        globalSetup: './test/global_setup.ts',
+        globalSetup: './backend/test/global_setup.ts',
     },
 });
